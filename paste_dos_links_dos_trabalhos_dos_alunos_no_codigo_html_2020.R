23efd40f4f952522f4d2503daea8ad718186399f
DATAUNIRIO <duttross@gmail.com>
2021-02-26 19:02:46 -0300

    2020.1

---
+++ paste_dos_links_dos_trabalhos_dos_alunos_no_codigo_html_2020.R
@@ -0,0 +1,28 @@
+library(readxl)
+nomes_e_links_rpubs<-read_xlsx("C:/Users/Hp/Google Drive (steven.ross@uniriotec.br)/2020/primeiro semestre COVID/NOTAS/notas_ADM_CP.xlsx",
+                               sheet="versao final",skip=1)
+
+nomes_e_links_rpubs<-nomes_e_links_rpubs[1:22,]
+names(nomes_e_links_rpubs)
+nomes_e_links_rpubs<-nomes_e_links_rpubs[,c("Nome","link")]
+nomes_e_links_rpubs$link[1]<-"https://rpubs.com/Andressachagas/"
+nomes_e_links_rpubs$link[20]<-"https://rpubs.com/ThamaraRodrigues"
+
+View(nomes_e_links_rpubs)
+
+nomes_e_links_rpubs<-nomes_e_links_rpubs[nomes_e_links_rpubs$link!="Sem registro",]
+nomes_e_links_rpubs<-na.omit(nomes_e_links_rpubs)
+
+SEQ  <- seq(1,33)
+pb   <- txtProgressBar(1, 33, style=3)
+dados<-c()
+#soh existe de menu 1 ate 9
+for(i in SEQ){
+  setTxtProgressBar(pb, i)
+  dados[i]<-paste0('<li class="menu-bar" id=','menu',sample(1:9,1),'">  <a href="',nomes_e_links_rpubs[i,2],
+    '"><br><br><img src="depoimentos.png" alt="',nomes_e_links_rpubs[i,1],'" title="',nomes_e_links_rpubs[i,1],'" />'
+    ,nomes_e_links_rpubs[i,1],
+    '</a>
+      </li>'
+)}
+
